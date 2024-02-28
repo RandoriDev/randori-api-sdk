3c77bc6da717a1413e6f9d2830385cfc7357e9db
BJ Swope <bj@randori.com>
2024-02-27 19:58:34 -0500

    latest tweaks

--- randori_api_sdk/api/cms_api.py
+++ randori_api_sdk/api/cms_api.py
@@ -234,57 +234,6 @@ class CmsApi(object):
             },
             api_client=api_client
         )
-        self.frontend_get_activity_configuration_endpoint = _Endpoint(
-            settings={
-                'response_type': (CmspbFrontendSingleConfigurationResponse,),
-                'auth': [
-                    'bearerAuth'
-                ],
-                'endpoint_path': '/cms/api/v1/frontend/activity-configurations/{id}',
-                'operation_id': 'frontend_get_activity_configuration',
-                'http_method': 'GET',
-                'servers': None,
-            },
-            params_map={
-                'all': [
-                    'id',
-                ],
-                'required': [
-                    'id',
-                ],
-                'nullable': [
-                ],
-                'enum': [
-                ],
-                'validation': [
-                ]
-            },
-            root_map={
-                'validations': {
-                },
-                'allowed_values': {
-                },
-                'openapi_types': {
-                    'id':
-                        (str,),
-                },
-                'attribute_map': {
-                    'id': 'id',
-                },
-                'location_map': {
-                    'id': 'path',
-                },
-                'collection_format_map': {
-                }
-            },
-            headers_map={
-                'accept': [
-                    'application/json'
-                ],
-                'content_type': [],
-            },
-            api_client=api_client
-        )
         self.frontend_get_configuration_criteria_endpoint = _Endpoint(
             settings={
                 'response_type': (CmspbFrontendSingleTriggerResponse,),
@@ -730,6 +679,57 @@ class CmsApi(object):
             },
             api_client=api_client
         )
+        self.frontend_list_single_activity_configuration_endpoint = _Endpoint(
+            settings={
+                'response_type': (CmspbFrontendSingleConfigurationResponse,),
+                'auth': [
+                    'bearerAuth'
+                ],
+                'endpoint_path': '/cms/api/v1/frontend/activity-configurations/{id}',
+                'operation_id': 'frontend_list_single_activity_configuration',
+                'http_method': 'GET',
+                'servers': None,
+            },
+            params_map={
+                'all': [
+                    'id',
+                ],
+                'required': [
+                    'id',
+                ],
+                'nullable': [
+                ],
+                'enum': [
+                ],
+                'validation': [
+                ]
+            },
+            root_map={
+                'validations': {
+                },
+                'allowed_values': {
+                },
+                'openapi_types': {
+                    'id':
+                        (str,),
+                },
+                'attribute_map': {
+                    'id': 'id',
+                },
+                'location_map': {
+                    'id': 'path',
+                },
+                'collection_format_map': {
+                }
+            },
+            headers_map={
+                'accept': [
+                    'application/json'
+                ],
+                'content_type': [],
+            },
+            api_client=api_client
+        )
         self.frontend_validate_now_endpoint = _Endpoint(
             settings={
                 'response_type': (CmspbFrontendRunNowJobResponse,),
@@ -1038,84 +1038,6 @@ class CmsApi(object):
         kwargs['cms_update_parameter_request'] = \
             cms_update_parameter_request
         return self.frontend_edit_activity_configuration_parameter_endpoint.call_with_http_info(**kwargs)
-
-    def frontend_get_activity_configuration(
-        self,
-        id,
-        **kwargs
-    ):
-        """Get activity configurations  # noqa: E501
-
-        Get activity configurations  # noqa: E501
-        This method makes a synchronous HTTP request by default. To make an
-        asynchronous HTTP request, please pass async_req=True
-
-        >>> thread = api.frontend_get_activity_configuration(id, async_req=True)
-        >>> result = thread.get()
-
-        Args:
-            id (str): Activity Configuration ID
-
-        Keyword Args:
-            _return_http_data_only (bool): response data without head status
-                code and headers. Default is True.
-            _preload_content (bool): if False, the urllib3.HTTPResponse object
-                will be returned without reading/decoding response data.
-                Default is True.
-            _request_timeout (int/float/tuple): timeout setting for this request. If
-                one number provided, it will be total request timeout. It can also
-                be a pair (tuple) of (connection, read) timeouts.
-                Default is None.
-            _check_input_type (bool): specifies if type checking
-                should be done one the data sent to the server.
-                Default is True.
-            _check_return_type (bool): specifies if type checking
-                should be done one the data received from the server.
-                Default is True.
-            _spec_property_naming (bool): True if the variable names in the input data
-                are serialized names, as specified in the OpenAPI document.
-                False if the variable names in the input data
-                are pythonic names, e.g. snake case (default)
-            _content_type (str/None): force body content-type.
-                Default is None and content-type will be predicted by allowed
-                content-types and body.
-            _host_index (int/None): specifies the index of the server
-                that we want to use.
-                Default is read from the configuration.
-            async_req (bool): execute request asynchronously
-
-        Returns:
-            CmspbFrontendSingleConfigurationResponse
-                If the method is called asynchronously, returns the request
-                thread.
-        """
-        kwargs['async_req'] = kwargs.get(
-            'async_req', False
-        )
-        kwargs['_return_http_data_only'] = kwargs.get(
-            '_return_http_data_only', True
-        )
-        kwargs['_preload_content'] = kwargs.get(
-            '_preload_content', True
-        )
-        kwargs['_request_timeout'] = kwargs.get(
-            '_request_timeout', None
-        )
-        kwargs['_check_input_type'] = kwargs.get(
-            '_check_input_type', True
-        )
-        kwargs['_check_return_type'] = kwargs.get(
-            '_check_return_type', True
-        )
-        kwargs['_spec_property_naming'] = kwargs.get(
-            '_spec_property_naming', False
-        )
-        kwargs['_content_type'] = kwargs.get(
-            '_content_type')
-        kwargs['_host_index'] = kwargs.get('_host_index')
-        kwargs['id'] = \
-            id
-        return self.frontend_get_activity_configuration_endpoint.call_with_http_info(**kwargs)
 
     def frontend_get_configuration_criteria(
         self,
@@ -1690,6 +1612,84 @@ class CmsApi(object):
         kwargs['entity_type'] = \
             entity_type
         return self.frontend_list_applicable_entities_parameters_endpoint.call_with_http_info(**kwargs)
+
+    def frontend_list_single_activity_configuration(
+        self,
+        id,
+        **kwargs
+    ):
+        """Get activity configurations  # noqa: E501
+
+        Get activity configurations  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+
+        >>> thread = api.frontend_list_single_activity_configuration(id, async_req=True)
+        >>> result = thread.get()
+
+        Args:
+            id (str): Activity Configuration ID
+
+        Keyword Args:
+            _return_http_data_only (bool): response data without head status
+                code and headers. Default is True.
+            _preload_content (bool): if False, the urllib3.HTTPResponse object
+                will be returned without reading/decoding response data.
+                Default is True.
+            _request_timeout (int/float/tuple): timeout setting for this request. If
+                one number provided, it will be total request timeout. It can also
+                be a pair (tuple) of (connection, read) timeouts.
+                Default is None.
+            _check_input_type (bool): specifies if type checking
+                should be done one the data sent to the server.
+                Default is True.
+            _check_return_type (bool): specifies if type checking
+                should be done one the data received from the server.
+                Default is True.
+            _spec_property_naming (bool): True if the variable names in the input data
+                are serialized names, as specified in the OpenAPI document.
+                False if the variable names in the input data
+                are pythonic names, e.g. snake case (default)
+            _content_type (str/None): force body content-type.
+                Default is None and content-type will be predicted by allowed
+                content-types and body.
+            _host_index (int/None): specifies the index of the server
+                that we want to use.
+                Default is read from the configuration.
+            async_req (bool): execute request asynchronously
+
+        Returns:
+            CmspbFrontendSingleConfigurationResponse
+                If the method is called asynchronously, returns the request
+                thread.
+        """
+        kwargs['async_req'] = kwargs.get(
+            'async_req', False
+        )
+        kwargs['_return_http_data_only'] = kwargs.get(
+            '_return_http_data_only', True
+        )
+        kwargs['_preload_content'] = kwargs.get(
+            '_preload_content', True
+        )
+        kwargs['_request_timeout'] = kwargs.get(
+            '_request_timeout', None
+        )
+        kwargs['_check_input_type'] = kwargs.get(
+            '_check_input_type', True
+        )
+        kwargs['_check_return_type'] = kwargs.get(
+            '_check_return_type', True
+        )
+        kwargs['_spec_property_naming'] = kwargs.get(
+            '_spec_property_naming', False
+        )
+        kwargs['_content_type'] = kwargs.get(
+            '_content_type')
+        kwargs['_host_index'] = kwargs.get('_host_index')
+        kwargs['id'] = \
+            id
+        return self.frontend_list_single_activity_configuration_endpoint.call_with_http_info(**kwargs)
 
     def frontend_validate_now(
         self,
