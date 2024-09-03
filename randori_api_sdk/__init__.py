8af969b57f9ea153f352e046409ac93f2e507686
randori-ckina <99660439+randori-ckina@users.noreply.github.com>
2024-09-03 20:52:41 +0000

    auto-update

--- randori_api_sdk/__init__.py
+++ randori_api_sdk/__init__.py
@@ -11,7 +11,7 @@
 """
 
 
-__version__ = ""
+__version__ = "2.0.0"
 
 # import ApiClient
 from randori_api_sdk.api_client import ApiClient
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "randori-api-sdk"
-VERSION = ""
+VERSION = "2.0.0"
 # To install the library, run the following
 #
 # python setup.py install
